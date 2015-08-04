faf190013649b8e98f50f132b2af91e73ab8c238
zfayaz <diam00nd>
2015-08-04 15:45:55 -0400

    date selector now works, had to set min and max date limit.

--- app/src/main/java/edu/psu/ist402/projectfittness/UserEntryActivity.java
+++ app/src/main/java/edu/psu/ist402/projectfittness/UserEntryActivity.java
@@ -78,18 +78,23 @@ public class UserEntryActivity extends Activity {
         public Dialog onCreateDialog(Bundle savedInstanceState) {
             // Use the current date as the default date in the picker
             final Calendar c = Calendar.getInstance();
-            int year = c.get(Calendar.YEAR) - 21;
+            int year = c.get(Calendar.YEAR);
             int month = c.get(Calendar.MONTH);
             int day = c.get(Calendar.DAY_OF_MONTH);
 
             // Create a new instance of DatePickerDialog and return it
-            return new DatePickerDialog(getActivity(), this, year, month, day);
+            DatePickerDialog dpd = new DatePickerDialog(getActivity(), this, year-21, month, day);
+            dpd.getDatePicker().setMinDate(dpd.getDatePicker().getMinDate());
+            dpd.getDatePicker().setMaxDate(c.getTimeInMillis());
+            return dpd;
         }
 
         @Override
         public void onDateSet(DatePicker view, int year, int month, int day) {
             //blah
         }
+
+
 
 
     }
