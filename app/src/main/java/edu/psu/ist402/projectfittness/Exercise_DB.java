bf4ec2d93ba0d944a46b8dccd2a0831571ba027b
mjs330 <mvs5709@psu.edu>
2015-08-06 09:47:20 -0400

    updated user DAO

--- app/src/main/java/edu/psu/ist402/projectfittness/Exercise_DB.java
+++ app/src/main/java/edu/psu/ist402/projectfittness/Exercise_DB.java
@@ -1,10 +1,10 @@
 package edu.psu.ist402.projectfittness;
 
 import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 
 import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
-
-import java.util.Date;
 
 /**
  * Created by Marc on 8/6/2015.
@@ -58,31 +58,95 @@ public class Exercise_DB extends SQLiteAssetHelper {
 
 
     //User DAO
-    //TODO
-
-    // Update User
+       // Update User
     public void updateUser(String Name, Date Birthdate, float height, float weight) {
+    public void updateUser(String name, int birthdate, float height, float weight) {
+        SQLiteDatabase db = this.getWritableDatabase();
 
+        //Delete existing data
+        String sql = "DELETE FROM " + TABLE_USER;
+        db.execSQL(sql);
+
+        //Add user
+        sql = "INSERT INTO " + TABLE_USER
+                + " (" + COLUMN_NAME + ", " + COLUMN_BIRTHDATE + ", " + COLUMN_HEIGHT + ", " + COLUMN_WEIGHT
+                + ") VALUES('" + name + "', " + birthdate + ", " + height + ", " + weight + ")";
+        db.execSQL(sql);
+
+        db.close();
     }
 
     // Get UserName
     public String getUserName() {
         return "";
+        String name =  "";
+
+        String selectQuery = "SELECT * FROM " + TABLE_USER;
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery(selectQuery, null);
+        if (cursor.moveToFirst()) {
+            do {
+                name = cursor.getString(cursor.getColumnIndex(COLUMN_NAME));
+            } while (cursor.moveToNext());
+        }
+        db.close();
+
+        return name;
     }
 
     // Get UserBirthdate
     public String getUserBirthdate() {
         return "";
+    public int getUserBirthdate() {
+        int birthdate = 0;
+
+        String selectQuery = "SELECT * FROM " + TABLE_USER;
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery(selectQuery, null);
+        if (cursor.moveToFirst()) {
+            do {
+                birthdate = cursor.getInt(cursor.getColumnIndex(COLUMN_BIRTHDATE));
+            } while (cursor.moveToNext());
+        }
+        db.close();
+
+        return birthdate;
     }
 
     // Get UserHeight
     public float getUserHeight() {
         return 0;
+        float height = 0;
+
+        String selectQuery = "SELECT * FROM " + TABLE_USER;
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery(selectQuery, null);
+        if (cursor.moveToFirst()) {
+            do {
+                height = cursor.getInt(cursor.getColumnIndex(COLUMN_HEIGHT));
+            } while (cursor.moveToNext());
+        }
+        db.close();
+
+        return height;
     }
 
     // Get UserWeight
     public float getUserWeight() {
         return 0;
+        float weight = 0;
+
+        String selectQuery = "SELECT * FROM " + TABLE_USER;
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery(selectQuery, null);
+        if (cursor.moveToFirst()) {
+            do {
+                weight = cursor.getInt(cursor.getColumnIndex(COLUMN_WEIGHT));
+            } while (cursor.moveToNext());
+        }
+        db.close();
+
+        return weight;
     }
 
 
